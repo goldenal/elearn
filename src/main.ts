b3affd08b458bb87ba4a91baabe388a904642794
Adewale <adeosunadewale86@gmail.com>
2025-10-21 22:57:24 +0100

    fix: simplify server initialization by removing ExpressAdapter and unnecessary server setup

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
-import { ExpressAdapter } from '@nestjs/platform-express';
-
-const express = require('express');
 
 async function bootstrap() {
-  const server = express();
-  
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  const app = await NestFactory.create(AppModule);
 
   app.useGlobalInterceptors(new LoggingInterceptor());
   app.enableCors();
@@ -30,8 +25,6 @@ async function bootstrap() {
   SwaggerModule.setup('api/docs', app, document, {
     swaggerOptions: { persistAuthorization: true },
   });
-
-  await app.init();
 
   const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
